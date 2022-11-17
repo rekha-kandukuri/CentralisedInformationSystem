a44b55b9f8db9cb466f5eefe0023cf742c98949f
rekha-kandukuri <kandukurirekha14@gmail.com>
2022-11-17 10:40:44 +0530

    Reset Templates Folder Recommender

--- app/recommender/__init__.py
+++ app/recommender/__init__.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
 
-recommender = Blueprint('recommender', __name__, template_folder='templates')
+recommender = Blueprint('recommender', __name__)
 
 from . import routes, models
