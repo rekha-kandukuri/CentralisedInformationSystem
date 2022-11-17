b4d9fd668607d779fc167de0c87da414998223e0
rekha-kandukuri <kandukurirekha14@gmail.com>
2022-11-17 10:35:47 +0530

    Register Blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,42 @@
-from flask import Flask, redirect, render_template, url_for
+from flask import Flask
 from config import config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_socketio import SocketIO, send
 
 
+# general configs
 app = Flask(__name__)
 app.config.from_object(config)
+socketio = SocketIO(app, engineio_logger=True, logger=True)
 
 
-# DB Config
+# DB Configs
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 
-# Login Configuration
+# Login Configs
 login = LoginManager(app)
 login.login_view = 'auth.loginUser'
 login.message = 'Please Login To Access The Page'
 login.login_message_category = 'info'
+
+
+# Blueprint Registrations
+from app.auth import auth
+from app.course import course
+from app.recommender import recommender
+from app.forum import forum
+from app.home import home
+
+app.register_blueprint(auth, url_prefix='/auth')
+app.register_blueprint(course, url_prefix='/course')
+app.register_blueprint(recommender, url_prefix='/recommender')
+app.register_blueprint(forum, url_prefix='/forum')
+app.register_blueprint(home, url_prefix='')
+
+from .utils import errorhandlers
+from .admin import Admin
+from .mailconfig import mail
--- cis.py
+++ cis.py
@@ -1,5 +1,14 @@
-from app import app
+from app import app, db, socketio
+from app.forum.models import thread, post
+from app.course.models import Courses
+from app.auth.models import User
+
+
+# Flask Shell Command Gives Access to DataBase Manipulation Facilated Through Flask SQLAlchemy
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'thread': thread, 'post': post, 'course': Courses}
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app, debug=True, log_output=True, port=8000)
