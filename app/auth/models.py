791f4770ac55d5c44630e6ee746a44089102bc05
rekha-kandukuri <kandukurirekha14@gmail.com>
2022-11-17 10:53:40 +0530

    Update GitIgnore Remove Migrations, db

--- app/auth/models.py
+++ app/auth/models.py
@@ -3,7 +3,7 @@ from app import db, app, login
 from flask_login import UserMixin
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
-from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from itsdangerous import URLSafeTimedSerializer as Serializer
 
 
 @login.user_loader
