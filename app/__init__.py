c5388341d3a82aef644e5025cdda96f6819f80df
rekha-kandukuri <kandukurirekha14@gmail.com>
2022-11-17 08:04:14 +0530

    DB, Login Config

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,21 @@
 from flask import Flask, redirect, render_template, url_for
 from config import config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+
 
 app = Flask(__name__)
 app.config.from_object(config)
 
-@app.route('/')
-def home():
-    return "Home"
+
+# DB Config
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+# Login Configuration
+login = LoginManager(app)
+login.login_view = 'auth.loginUser'
+login.message = 'Please Login To Access The Page'
+login.login_message_category = 'info'
